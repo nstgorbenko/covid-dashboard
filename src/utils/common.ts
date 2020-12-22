f0a6cc621a15a356b4672e6fa5e227a27d4af352
Anastasiia Gorbenko <nstgorbenko@yandex.ru>
2020-12-22 16:52:37 +0300

    feat: add time of last update

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
-import { Screen } from '@/constants/constants';
 import { CSSProperties } from 'react';
+
+import { Screen } from '@/constants/constants';
 
 export const getNextArrayItem = <T>(array: Array<T>, currentItem: T): T => {
   const currentItemIndex = array.indexOf(currentItem);
@@ -18,11 +19,36 @@ export const getPreviousArrayItem = <T>(array: Array<T>, currentItem: T): T => {
   return array[currentItemIndex - 1];
 };
 
-export const getScreenComponentClass = (screenComponentName: Screen, isFullScreenComponent: boolean, fullScreenName: Screen, styles: CSSProperties) => {
+export const getScreenComponentClass = (
+  screenComponentName: Screen,
+  isFullScreenComponent: boolean,
+  fullScreenName: Screen,
+  styles: CSSProperties
+): string => {
   if (isFullScreenComponent) {
-    return classNames(styles[screenComponentName], styles['grid__element'], styles['grid__element--show']);
-  } else if (!isFullScreenComponent && fullScreenName !== Screen.ALL) {
-    return classNames(styles[screenComponentName], styles['grid__element'], styles['grid__element--hide']);
+    return classNames(
+      styles[screenComponentName],
+      styles['grid__element'],
+      styles['grid__element--show']
+    );
+  } if (!isFullScreenComponent && fullScreenName !== Screen.ALL) {
+    return classNames(
+      styles[screenComponentName],
+      styles['grid__element'],
+      styles['grid__element--hide']
+    );
   }
   return classNames(styles[screenComponentName], styles['grid__element']);
+};
+
+export const formatDate = (date: number): string => {
+  const ISODate = new Date(date).toISOString();
+
+  const day = ISODate.slice(8, 10);
+  const month = ISODate.slice(5, 7);
+  const year = ISODate.slice(0, 4);
+  const hours = ISODate.slice(11, 13);
+  const minutes = ISODate.slice(14, 16);
+
+  return `${day}/${month}/${year}, ${hours}:${minutes}`;
 };
