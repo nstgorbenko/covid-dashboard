e812d68a621fc2aa27e8969025ec222652114a6d
Anastasiia Gorbenko <nstgorbenko@yandex.ru>
2020-12-22 16:23:55 +0300

    feat: implement table component full screen view

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,35 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Dispatch } from 'redux';
 import {connect} from "react-redux";
-import classNames from 'classnames';
 import styles from './Table.scss';
 import TableRow from '@/components/TableRow';
 import Resize from '@/components/Resize';
 import Title from '@/components/Title';
 import { Parameter } from '@/constants/constants';
 import { getGlobalData, getCountriesData } from '@/store/data/selector';
-import { getCountry, getParameter } from '@/store/app/selector';
+import { getActiveScreen, getCountry, getParameter } from '@/store/app/selector';
 import { CountryDataInterface, StateInterface, GlobalDataInterface } from '@/types/entities';
 import getShownTableData from '@/utils/table-data';
 import { Screen } from '@/constants/constants';
+import { getScreenComponentClass } from '@/utils/common';
+import { ActionCreator } from '@/store/app/app';
 
 interface TableProps {
+  fullScreen: Screen;
   country: string;
   parameter: Parameter;
   countriesData: Array<CountryDataInterface>;
   globalData: GlobalDataInterface;
+  changeActiveScreen(screen: Screen): void;
 }
 
 const Table: React.FC<TableProps> = (props: TableProps) => {
-  const { country, parameter, countriesData, globalData } = props;
+  const { fullScreen, country, parameter, countriesData, globalData, changeActiveScreen } = props;
   const shownData = getShownTableData(globalData, countriesData, country, parameter);
+  const screenName = Screen.TABLE;
+
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const changeScreenView = () => {
+    isFullScreen ? changeActiveScreen(Screen.ALL) : changeActiveScreen(screenName);
+    setIsFullScreen(prev => !prev);
+  };
 
   return (
-    <div className={classNames(
-      styles['table'],
-      styles['grid__element']
-    )}>
-      <Resize isFullScreen={false} onClick={() => {}}/>
-      <Title screen={Screen.TABLE}/>
+    <div className={getScreenComponentClass(screenName, isFullScreen, fullScreen, styles)}>
+      <Resize isFullScreen={isFullScreen} onClick={changeScreenView}/>
+      <Title screen={screenName}/>
       <table className={styles['table__content']}>
         <tbody>
           {shownData.map((data) =>
@@ -45,10 +53,17 @@ const Table: React.FC<TableProps> = (props: TableProps) => {
 };
 
 const mapStateToProps = (state: StateInterface) => ({
+  fullScreen: getActiveScreen(state),
   country: getCountry(state),
   parameter: getParameter(state),
   countriesData: getCountriesData(state),
   globalData: getGlobalData(state),
 });
 
-export default connect(mapStateToProps)(Table);
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  changeActiveScreen(screen: Screen) {
+    dispatch(ActionCreator.changeActiveScreen(screen));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Table);
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,7 @@
+import classNames from 'classnames';
+import { Screen } from '@/constants/constants';
+import { CSSProperties } from 'react';
+
 export const getNextArrayItem = <T>(array: Array<T>, currentItem: T): T => {
   const currentItemIndex = array.indexOf(currentItem);
   if (currentItemIndex === array.length - 1) {
@@ -12,4 +16,13 @@ export const getPreviousArrayItem = <T>(array: Array<T>, currentItem: T): T => {
     return array[array.length - 1];
   }
   return array[currentItemIndex - 1];
+};
+
+export const getScreenComponentClass = (screenComponentName: Screen, isFullScreenComponent: boolean, fullScreenName: Screen, styles: CSSProperties) => {
+  if (isFullScreenComponent) {
+    return classNames(styles[screenComponentName], styles['grid__element'], styles['grid__element--show']);
+  } else if (!isFullScreenComponent && fullScreenName !== Screen.ALL) {
+    return classNames(styles[screenComponentName], styles['grid__element'], styles['grid__element--hide']);
+  }
+  return classNames(styles[screenComponentName], styles['grid__element']);
 };
